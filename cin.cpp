c9f687e42ad43b6ab761b1abe0316dae6a22b21a
Palak <kanwarpalak790@gmail.com>
2025-12-28 04:20:28 +0000

    Intial commit

---
+++ cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2;
+
+        cout << "enter the num1!";/*insertion operator */
+        cin>> num1;/*extraction operator*/
+
+         cout <<"enter the num2";
+         cin>> num2;
+	 cout<<"the sum of the no. is :" <<num1+num2;
+         return 0;
+}
+
